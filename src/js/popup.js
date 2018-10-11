52221705b51bd75a286c3a9283a160c51e1eacb7
mengdu <1752295326@qq.com>
2018-10-11 23:46:31 +0800

    完成布局

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,57 +1,31 @@
 (function () {
-  // var extensionId = 'llhdjeloimohpfbkmboaepoidgpcgpbp'
+  var privateKey = 'kZTxYGntfMjeGA_Lf42k'
   var app = new Vue({
     el: '#root',
     data: {
-      self: null,
-      query: '',
-      extensions: []
+      query: {
+        q: '',
+        from: 'en',
+        to: 'zh',
+        appid: '20181011000218111',
+        salt: '',
+        sign: ''
+      },
+      result: "This is a test"
     },
     computed: {
-      filterExtensions () {
-        var that = this
-        if (!this.self) return this.extensions
-        var list = this.extensions.filter(function (item) {
-          // return item.id !== extensionId
-          return item.id !== that.self.id
-        })
-        if (!this.query) return list
-
-        list = list.filter(function (item) {
-          return item.name.indexOf(that.query) > -1
-        })
-
-        return list
-      }
+      
     },
     methods: {
-      enabledChange (index, item) {
-        // console.log(index, item.enabled, item)
-        chrome.management.setEnabled(item.id, item.enabled)
-      },
-      handleExec (index, item) {
-        chrome.management.launchApp(item.id)
+      handleTranslate () {
+        this.query.salt = new Date().getTime() + '' + Math.random()
+        this.query.sign = MD5(this.query.appid + this.query.q + this.query.salt + privateKey)
+        this.result = this.query
+        console.log(JSON.parse(JSON.stringify(this.query)))
       }
     },
     created: function () {
       var that = this
-      // 获取本扩展信息
-      chrome.management.getSelf(function (ex) {
-        that.self = ex
-      })
-      // 获取扩展列表
-      chrome.management.getAll(function (list) {
-        // console.log(list)
-        that.extensions = list.sort(function (a, b) {
-          if (a.enabled && b.enabled) {
-            return 0
-          } else if (!a.enabled && b.enabled) {
-            return 1
-          } else {
-            return -1
-          }
-        })
-      })
     }
   })
 
